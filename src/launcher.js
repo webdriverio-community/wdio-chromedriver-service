4696bf5c502f4ea5a82443676e67ab1f1befcf84
Erwin Heitzman <erwin.heitzman@gmail.com>
2020-04-01 21:53:10 +0200

    chromedriver: make v6 compatible

--- src/launcher.js
+++ src/launcher.js
@@ -3,42 +3,55 @@ import ChromeDriver from 'chromedriver'
 
 import getFilePath from './utils/getFilePath'
 
-const DEFAULT_LOG_FILENAME = 'ChromeDriver.txt'
+const DEFAULT_LOG_FILENAME = 'chromedriver.log'
+
+const DEFAULT_CONNECTION = {
+    protocol: 'http',
+    hostname: 'localhost',
+    port: 9515,
+    path: '/'
+}
+
+const isMultiremote = obj => typeof obj === 'object' && !Array.isArray(obj)
+const isChrome = cap => cap.browserName.toLowerCase() === 'chrome'
 
 export default class ChromeDriverLauncher {
-    constructor () {
-        this.chromeDriverLogs = null
-        this.chromeDriverArgs = null
-        this.logToStdout = false
+    constructor(options, capabilities, config) {
+        this.options = {
+            protocol: options.protocol || DEFAULT_CONNECTION.protocol,
+            hostname: options.hostname || DEFAULT_CONNECTION.hostname,
+            port: options.port || DEFAULT_CONNECTION.port,
+            path: options.path || DEFAULT_CONNECTION.path,
+        }
 
-        return this
+        this.outputDir = options.outputDir || config.outputDir
+        this.capabilities = capabilities
+        this.chromeDriverArgs = []
     }
 
-    async onPrepare (config) {
-        this.chromeDriverArgs = config.chromeDriverArgs || []
-        this.chromeDriverLogs = config.chromeDriverLogs
+    async onPrepare() {
+        this.chromeDriverArgs.push(`--port=${this.options.port}`)
+        this.chromeDriverArgs.push(`--url-base=${this.options.path}`)
 
-        if (!this.chromeDriverArgs.find(arg => arg.startsWith('--port'))) {
-            this.chromeDriverArgs.push(`--port=${config.port}`)
-        }
+        /**
+         * update capability connection options to connect
+         * to chromedriver
+         */
+        this._mapCapabilities()
 
-        if (!this.chromeDriverArgs.find(arg => arg.startsWith('--url-base'))) {
-            this.chromeDriverArgs.push(`--url-base=${config.path}`)
-        }
-        
         this.process = await ChromeDriver.start(this.chromeDriverArgs, true)
 
-        if (typeof this.chromeDriverLogs === 'string') {
+        if (typeof this.outputDir === 'string') {
             this._redirectLogStream()
         }
     }
 
-    onComplete () {
+    onComplete() {
         ChromeDriver.stop()
     }
 
-    _redirectLogStream () {
-        const logFile = getFilePath(this.chromeDriverLogs, DEFAULT_LOG_FILENAME)
+    _redirectLogStream() {
+        const logFile = getFilePath(this.outputDir, DEFAULT_LOG_FILENAME)
 
         // ensure file & directory exists
         fs.ensureFileSync(logFile)
@@ -46,5 +59,21 @@ export default class ChromeDriverLauncher {
         const logStream = fs.createWriteStream(logFile, { flags: 'w' })
         this.process.stdout.pipe(logStream)
         this.process.stderr.pipe(logStream)
+    }
+
+    _mapCapabilities() {
+        if (isMultiremote(this.capabilities)) {
+            for (const cap in this.capabilities) {
+                if (isChrome(this.capabilities[cap])) {
+                    Object.assign(this.capabilities[cap], this.options)
+                }
+            }
+        } else {
+            for (const cap of this.capabilities) {
+                if (isChrome(cap)) {
+                    Object.assign(cap, this.options)
+                }
+            }
+        }
     }
 }
